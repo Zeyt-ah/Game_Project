08c07a5d548f37129b27678644c8375305b8e1fa
Zeyt-ah <kamil.wojkowski1@gmail.com>
2026-03-01 17:26:19 +0000

    spell added
    - pressing F launches a fire spell

--- Fantasy_Game/Assets/Scripts/PlayerScripts/PlayerAnimationScript.cs
+++ Fantasy_Game/Assets/Scripts/PlayerScripts/PlayerAnimationScript.cs
@@ -22,6 +22,7 @@ public class PlayerAnimationScript : MonoBehaviour
 
         //event listeners
         combatScript.OnAttackStarted += AttackAnimation;
+        combatScript.OnCastStarted += CastAnimation;
         interactionScript.OnInteractStarted += InteractAnimation;
         movementScript.OnJumpStarted += JumpAnimation;
         movementScript.OnClimbStarted += ClimbAnimation;
@@ -50,6 +51,10 @@ public class PlayerAnimationScript : MonoBehaviour
     private void AttackAnimation()
     {
         animator.SetTrigger("Attack");
+    }
+    private void CastAnimation()
+    {
+        animator.SetTrigger("castSpell");
     }
 
     private void InteractAnimation()
--- Fantasy_Game/Assets/Scripts/PlayerScripts/PlayerCombatScript.cs
+++ Fantasy_Game/Assets/Scripts/PlayerScripts/PlayerCombatScript.cs
@@ -8,10 +8,13 @@ public class PlayerCombatScript : MonoBehaviour
 
     public BoxCollider _attackHitbox;
 
-    [SerializeField] private float attackTime = 0.9f;
+    public GameObject fireSpell;
 
+    [SerializeField] private float attackTime = 0.9f;
+    [SerializeField] private float spellTime = 2f;
 
     public System.Action OnAttackStarted;
+    public System.Action OnCastStarted;
 
 
 
@@ -32,7 +35,18 @@ public class PlayerCombatScript : MonoBehaviour
             StartCoroutine(Attacking());
         }
 
+    }
 
+    public void CastSpell(InputAction.CallbackContext context)
+    {
+        if (!context.started || !player.CanAttack() || player.IsDead()) return;
+        if (player._characterController.isGrounded)
+        {
+            player.DisableMovement();
+            OnCastStarted?.Invoke();
+
+            StartCoroutine(CastingSpell());
+        }
     }
 
     IEnumerator Attacking()
@@ -50,4 +64,39 @@ public class PlayerCombatScript : MonoBehaviour
         _attackHitbox.enabled = true;
     }
 
+
+    //for spells
+    IEnumerator CastingSpell()
+    {
+        StartCoroutine(SpellHitboxOn());
+        yield return new WaitForSeconds(spellTime);
+        player.EnableMovement();
+    }
+
+    //waits a bit to enable attack hitbox
+    IEnumerator SpellHitboxOn()
+    {
+        yield return new WaitForSeconds(1.3f);
+        GameObject spell = Instantiate(fireSpell, transform.position, transform.rotation);
+        StartCoroutine(SpellMovement(spell));
+    }
+    IEnumerator SpellMovement(GameObject spell)
+    {
+        float speed = 15f;
+        float duration = 4f;
+        float elapsed = 0f;
+
+        Vector3 moveDirection = transform.forward;
+
+        while (elapsed < duration)
+        {
+            spell.transform.position += moveDirection * speed * Time.deltaTime; // move forward
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(spell); // remove spell after movement
+    }
+       
+        
 }
